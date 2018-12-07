74c6c85e2e60573966c8667a864dc1cbff020147
JuanHernandez2 <jf.hernandez@globant.com>
2018-12-07 16:57:11 -0500

    US-031 consumir json materias v2 hh:mm

--- Angular/src/app/class-modal/class-modal.component.ts
+++ Angular/src/app/class-modal/class-modal.component.ts
@@ -7,12 +7,37 @@ import { Subject } from '../shared/model/Subject';
 })
 export class ClassModalComponent implements OnInit {
 
+   private currentTime;
+   private futureTime;
+   private time;
   @Input() private subject: Subject;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { 
     this.subject = data.class;
   }
 
   ngOnInit() {
+   
   }
+
+  msToHMS( ms ) : string{
+    /*
+    this.currentTime = new Date().getTime();
+    this.futureTime = ms
+    this.time = this.futureTime - this.currentTime;
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = seconds / 3600; // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes =  seconds / 60 ; // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = seconds % 60;
+    */
+    return (ms/3600)+":00";
+}
+
+
+   
 
 }
