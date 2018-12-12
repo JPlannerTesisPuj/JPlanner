efd7ae7c2eb30cd6682eedca5fafd4e5aec16b07
Juan Sebastián Gómez <chevy.gom@gmail.com>
2018-12-11 22:34:24 -0500

    prueba
    prueba de subida nueva rama

---
+++ Test-DragAndDrop/README.md
@@ -0,0 +1,79 @@
+# JPlanner
+
+## SPRING INSTALLATION
+Entrar al siguiente link:
+-https://spring.io/tools
+
+En este van a encontrar diferentes opciones de descarga, Spring Tools 4 para Eclipse, Visual Studio Code o Atom IDE.
+Seleccionan la opción de Eclipse para el sistema operativo que tengan en 64-bit. (En mi caso fue Windows)
+
+Cuando descarguen el IDE, dentro de la carpeta hay un ejecutable que se llama "SpringToolSuite4". Si no tienen problemas,
+vayan a la línea que contiene una cara feliz :D.
+
+Si tienen problemas para ejecutarlo, por ejemplo, java was started but returned exit code=13. Es que no tienen configurado
+el jdk versión 64-bit, para eso tienen que entrar en el archivo "SpringToolSuite4.ini" que está en la carpte del IDE, que
+acaban de descargar y colocar lo siguiente:
+
+-startup
+plugins/org.eclipse.equinox.launcher_1.5.100.v20180827-1352.jar
+--launcher.library
+plugins/org.eclipse.equinox.launcher.win32.win32.x86_64_1.1.800.v20180827-1352
+-product
+org.springframework.boot.ide.branding.sts4
+--launcher.defaultAction
+openFile
+-vm                                                               <-Tienen que colocar estas dos lineas antes del -vmargs
+C:\Program Files\Java\jdk1.8.0_71\jre\bin\javaw.exe               <-Acá colocan la dirección de su jdk\jre\bin\javaw.exe
+                                                                    El que tengo ahí es en mi caso para que se guien.
+
+-vmargs
+-Dosgi.requiredJavaVersion=1.8
+--add-modules=ALL-SYSTEM
+-Xms40m
+-Xmx1200m
+
+ :D  Luego de esto, cuando logren ejecutar Spring, van a importar un proyecto Maven, y seleccionan la carpeta de este branch.
+
+Y listo!
+
+---
+
+## ANGULAR INSTALLATION
+
+Para la instalación y creación de un nuevo proyecto en Angular 6 se deben seguir las siguientes instrucciones:
+Descargar las siguientes aplicaciones
+Node js: https://nodejs.org/es/ descargar la version actual
+Git: https://git-scm.com/ ir al link de descarga segun sistema operativo
+En la siguiente pagina se encuentran los comandos mas importantes implementados por angular cli, el cual es un ayudante de creación y puesta en marcha de Angular https://cli.angular.io/.
+
+Tras descargar e instalar las herramientas(ambas no necesitan opciones de instalación en especifico) se instalará Angular cli con el comando
+
+
+- ```npm install -g @angular/cli```
+
+luego en consola nos dirijimos al sitio en el cual se desea crear un nuevo proyecto por ejemplo
+
+### Para abrir el proyecto ya existente
+
+Abrir la carpeta Angular, abrir la terminal en dicha ubicación y ejecutar:
+
+- ```npm install``` se esperan unos minutos hasta que en la consola se indique que la instalación terminó, después digitamos
+
+- ```npm serve --open``` en consola se nos informará en que puerto esta situado el servidor, en este caso http://localhost:4200, y este se abrirá automáticamente en el navegador por defecto
+
+### Para crear un nuevo proyecto
+
+cd Desktop
+se nos creará en el escritorio, digitar el siguiente comando
+
+- ```ng new my-dream-app```
+se esperan unos minutos y las consola nos informa que fue creado satisfactoriamente, Angular posee un servidor interno el cual nos permite correr nuestra aplicación, para ponerlo en marcha digitamos
+
+- ```ng serve```
+
+en consola se nos informará en que puerto esta situado el servidor ejemplo
+http://localhost:4200
+Para más información dirigirse a -- https://cli.angular.io/ -- https://github.com/angular/angular-cli/wiki
+Curso de Angular 6 : https://www.youtube.com/watch?v=AR1tLGQ7COs&index=3&list=LLPeAk0L7s60ebYx6LiP9fOA&t=1880s
+
+
---
+++ Test-DragAndDrop/Spring/src/main/java/com/example/demo/services/JSONFileRestService.java
@@ -0,0 +1,95 @@
+package com.example.demo.services;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
+public class JSONFileRestService {
+
+	// Variable para armar un JSON con un error
+	Map<String, Object> errorResponse;
+
+	/**
+	 * Método que sirve para leer cualquier archivo JSON que esté ubicado en la carpeta src/main/resources/json y devolverlo como
+	 * un JSON mediante la petición de un método GET. El archivo que se leerá será el que pida el usuario por medio de la variable file_name.
+	 * 
+	 * La URL utilizada para hacer la petición es /files/read/json/{nombre del archivo}
+	 * 
+	 * @param file_name Nombre del archivo que se leerá
+	 * @return JSON con toda la información del archivo
+	 * @throws JsonProcessingException Manejador de error si no es posible generar armar una respuesta al no poder leer el archivo
+	 */
+	@RequestMapping(value = "files/read/json/{fileName}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	ResponseEntity<String> getJSONFile(@PathVariable("fileName") String fileName) throws JsonProcessingException {
+		
+		// Se obtiene la información del archivo
+		InputStream in = getClass().getResourceAsStream("/json/" + fileName + ".json");
+
+		// Si no existe el archivo se crea un JSON que contiene especificando el error
+		if (in == null) {
+			errorResponse = new HashMap<>();
+			errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+			errorResponse.put("error", "Error al abrir el archivo");
+			errorResponse.put("message", "No se ha encontrado el archivo " + fileName + ".json para leer");
+			errorResponse.put("path", "/files/read/json/" + fileName);
+			
+			// Convierte el Mapa con la especificación del error en un String en forma de JSON
+			String errorJson = new ObjectMapper().writeValueAsString(errorResponse);
+			
+			// Retorna un String en forma de JSON con un error 400 
+			return new ResponseEntity<>(errorJson, HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			// Lee el archivo 
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			StringBuilder JSONFileBuilder = new StringBuilder();
+
+			// Crea un String con toda la información del archivo JSON
+			String inputStringLine;
+			while ((inputStringLine = streamReader.readLine()) != null) {
+				JSONFileBuilder.append(inputStringLine);
+			}
+			
+			// Retorna el archivo JSON leído
+			return new ResponseEntity<>(JSONFileBuilder.toString(), HttpStatus.OK);
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		// Si hubo un error al leer el archivo se crea un JSON que contiene especificando el error
+		errorResponse = new HashMap<>();
+		errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+		errorResponse.put("error", "Error al abrir el archivo");
+		errorResponse.put("message", "No se pudo leer el archivo " + fileName + ".json para leer");
+		errorResponse.put("path", "/files/read/json/" + fileName);
+		
+		// Convierte el Mapa con la especificación del error en un String en forma de JSON
+		String errorJson = new ObjectMapper().writeValueAsString(errorResponse);
+		
+		// Retorna un String en forma de JSON con un error 400 
+		return new ResponseEntity<>(errorJson, HttpStatus.BAD_REQUEST);
+
+	}
+}
