2add680ae89b23731d1d605c8b6d4aaa9154f2d3
Jesúspinarte <jesuspinarte@gmail.com>
2018-12-13 11:07:32 -0500

    [US-033] Vista de Calendario semanal

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -8,14 +8,21 @@ import { AppComponent } from './app.component';
 import { DisplayClassesComponent } from './display-classes/display-classes.component';
 import { DisplayClassComponent } from './display-class/display-class.component';
 
-import {MatDialogModule } from '@angular/material';
+import { MatDialogModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ClassModalComponent } from './class-modal/class-modal.component';
 import { FilterComponent } from './filter/filter.component';
 
+// Libraries needed to display the calendar
+import { CommonModule } from '@angular/common';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+
 import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 import { DataService } from './shared/data.service';
+import { CalendarComponent } from './calendar/calendar.component';
 
 @NgModule({
   declarations: [
@@ -24,6 +31,7 @@ import { DataService } from './shared/data.service';
     DisplayClassComponent,
     ClassModalComponent,
     FilterComponent,
+    CalendarComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,8 +41,12 @@ import { DataService } from './shared/data.service';
     MatDialogModule,
     BrowserAnimationsModule,
     NgMultiSelectDropDownModule.forRoot(),
-  
-
+    CommonModule,
+    FlatpickrModule.forRoot(),
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory,
+    }),
   ],
   entryComponents: [
     ClassModalComponent,
