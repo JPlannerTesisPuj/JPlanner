e61a069760ea60e117060cde8f993bbc521beae3
JuanHernandez2 <jf.hernandez@globant.com>
2019-01-24 14:55:02 -0500

    Second Version Jsons and Scripts

--- Json Scripts/jsons_tesis.py
+++ Json Scripts/jsons_tesis.py
@@ -2,34 +2,41 @@ import json
 import requests
 import random
 
+#request the subjects json
 r = requests.get('https://trello-attachments.s3.amazonaws.com/5bd0e33922e9bc6cb9bd828c/5c4640a1afa3fd06460ab2fc/4914a3b27e4ac6534f355a49d763d6c6/classes.json')
 json_materias = r.json()
+#pensum json to return
 pensum = list()
 
 
 for i in range (0,80):
+
+    #Json that contains the pensum as a dictionary
     json_pensum = dict()
     
-    random_subject = random.randint(0,69)
-    json_pensum["_id"] = json_materias[random_subject]["_id"]
-    json_pensum["pre-requisitos"] = list()
+    random_subject_id = random.randint(0,69)
+    random_prev_subject = 100
+
+    #This is to verify if there's no subjects repeated
+    if random_subject_id != random_prev_subject:
+
+        #json_materias is a list, with dictionaries.
+        json_pensum["_id"] = json_materias[random_subject_id]["_id"]
+        random_prev_subject = random_subject_id
+    json_pensum["pre_requisitos"] = list()
+    for j in range(0,random.randint(0,15)):
+        id = random.randint(0,69)
+        previous_id = 100
+        #This is to verify if there's no subjects repeated
+        if id != previous_id:
+            json_pensum["pre_requisitos"].append(json_materias[id]["_id"])
+            previous_id = id 
     pensum.append(json_pensum)
 
-for s in pensum:
-    for k,v in s.items():
-        for i in range(0,random.randint(0,15)):
-            dict_pre = dict()
-            id = random.randint(0,69)
-            id_prev = 900000
-            if id != id_prev:
-                dict_pre["id_pre"] =  json_materias[id]["_id"]
-                id_prev = id 
-            s["pre-requisitos"].append(dict_pre)
+#For saving the .json file in the current path
+f= open("pensum_80.json","w+")
 
-print(json.dumps(pensum,sort_keys=True,indent=3))
-print(len(pensum))
-
-f= open("pensum.json","w+")
+#Transforming the list into a .json sintaxis
 f.write(json.dumps(pensum,sort_keys=True, indent=3))
 f.close()
 
--- Json Scripts/script_users.py
+++ Json Scripts/script_users.py
@@ -4,15 +4,19 @@ import random
 import string
 import jwt 
 def id_generator(some,payload):
-    """This is a function for retrieving a random string for student credentials"""
+    """This is a function for retrieving a JWT token for student credentials"""
     return jwt.encode({some: payload}, 'secret', algorithm='HS256')
 
+#Request the subjects json
 r = requests.get('https://trello-attachments.s3.amazonaws.com/5bd0e33922e9bc6cb9bd828c/5c4640a1afa3fd06460ab2fc/4914a3b27e4ac6534f355a49d763d6c6/classes.json')
 json_materias = r.json()
+
+#Taken subjects list
 users = list()
 
 
 for i in range (0,6):
+    #Dictionary that contains all the student information
     json_pensum = dict()
     json_pensum["nombre_estudiante"] = input("Nombre estudiante?")
     json_pensum["carrera"] = input("Carrea?")
@@ -21,12 +25,12 @@ for i in range (0,6):
     json_pensum["asignaturas_aprobadas"] = list()
     users.append(json_pensum)
 
-for s in users:
-    for k,v in s.items():
+for student in users:
+    for key,value in student.items():
         for i in range(0,random.randint(0,61)):
-            dict_pre = dict()
-            dict_pre["id_materia_cursada"] =  json_materias[random.randint(0,69)]["_id"] 
-            s["asignaturas_aprobadas"].append(dict_pre)
+            dict_classes_in_pensum = dict()
+            dict_classes_in_pensum["id_materia_cursada"] =  json_materias[random.randint(0,69)]["_id"] 
+            student["asignaturas_aprobadas"].append(dict_classes_in_pensum)
 
 print(json.dumps(users,sort_keys=True, indent=3))
 print(len(users))
