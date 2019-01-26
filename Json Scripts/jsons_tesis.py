027b6e794cb7a10c6c264016e4fc616683cb6366
JuanHernandez2 <jf.hernandez@globant.com>
2019-01-25 19:33:25 -0500

    New Json file and Python script

--- Json Scripts/jsons_tesis.py
+++ Json Scripts/jsons_tesis.py
@@ -7,26 +7,26 @@ r = requests.get('https://trello-attachments.s3.amazonaws.com/5bd0e33922e9bc6cb9
 json_materias = r.json()
 #pensum json to return
 pensum = list()
+previous_id = 100
+random_prev_subject = 100
 
 
 for i in range (0,80):
 
     #Json that contains the pensum as a dictionary
     json_pensum = dict()
-    
+
     random_subject_id = random.randint(0,69)
-    random_prev_subject = 100
 
     #This is to verify if there's no subjects repeated
     if random_subject_id != random_prev_subject:
-
         #json_materias is a list, with dictionaries.
         json_pensum["_id"] = json_materias[random_subject_id]["_id"]
         random_prev_subject = random_subject_id
     json_pensum["pre_requisitos"] = list()
     for j in range(0,random.randint(0,15)):
         id = random.randint(0,69)
-        previous_id = 100
+
         #This is to verify if there's no subjects repeated
         if id != previous_id:
             json_pensum["pre_requisitos"].append(json_materias[id]["_id"])
