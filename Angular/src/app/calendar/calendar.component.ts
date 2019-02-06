da7545a5d87ecb2993bde6055e0285cc261d3273
Nicolas <nicolas.restrepo@globant.com>
2019-02-06 17:33:20 -0500

    Fixing typos in confirmation message

--- Angular/src/app/calendar/calendar.component.ts
+++ Angular/src/app/calendar/calendar.component.ts
@@ -316,4 +316,9 @@ export class CalendarComponent implements OnInit {
 })
 export class OverlapClassConfirmationDialog {
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {}
+
+  private titleCaseWord(word: string) {
+    if (!word) return word;
+    return word[0].toUpperCase() + word.substr(1).toLowerCase();
+  }
 }
