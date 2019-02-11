a095c98443dfd813ec8dc0c93a5f31d1d2bb7d14
JuanHernandez2 <jf.hernandez@globant.com>
2019-02-11 14:22:35 -0500

    "Fixes json files and script"

--- Json Scripts/jsons_tesis.py
+++ Json Scripts/jsons_tesis.py
@@ -8,13 +8,14 @@ r = requests.get('https://trello-attachments.s3.amazonaws.com/5bd0e33922e9bc6cb9
 json_materias = r.json()
 #pensum json to return
 pensum = list()
+counter = 0
+random_subject_id = random.sample(range(69), 56)
 
-for i in range (0,80):
+for i in range (0,55):
     # Json that contains the pensum as a dictionary
     json_pensum = dict()
-    random_subject_id = random.randint(0,69)
     # json_materias is a list, with dictionaries.
-    json_pensum["_id"] = json_materias[random_subject_id]["_id"]
+    json_pensum["_id"] = json_materias[random_subject_id[counter]]["_id"]
     json_pensum["pre_requisitos"] = list()
     # This is to verify if there's no subjects repeated, returns a list with 15 numbers in a range
     # between 0 to 69
@@ -25,9 +26,11 @@ for i in range (0,80):
         else:
             json_pensum["pre_requisitos"].append(json_materias[id[j]]["_id"])
     pensum.append(json_pensum)
+    counter += 1
+   
 
 # For saving the .json file in the current path
-f= open("pensum_80.json","w+")
+f= open("pensum_55.json","w+")
 # Transforming the list into a .json sintaxis
 f.write(json.dumps(pensum,sort_keys=True, indent=3))
 f.close()
