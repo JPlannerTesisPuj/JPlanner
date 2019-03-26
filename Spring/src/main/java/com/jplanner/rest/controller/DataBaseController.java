dd7bc358c68c022742354646a2e92bae451214e6
Jesús <jesuspinarte@hotmail.com>
2019-03-25 19:14:16 -0500

    Finalización de métodos REST

--- Spring/src/main/java/com/jplanner/rest/controller/DataBaseController.java
+++ Spring/src/main/java/com/jplanner/rest/controller/DataBaseController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +25,7 @@ import com.jplanner.rest.model.Usuario;
 import com.jplanner.rest.model.key.AlternativaKey;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", allowCredentials = "true")
 public class DataBaseController {
 
 	// VARIABLES
