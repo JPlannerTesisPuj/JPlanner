55195915b5b6cb4ddceb66c7ffe65d078d1aaee5
Álvaro Pongutá <alvaro2102@hotmail.com>
2019-03-29 04:12:18 -0500

    FullModalTabletLaptop

--- Angular/src/app/class-modal/class-modal.component.ts
+++ Angular/src/app/class-modal/class-modal.component.ts
@@ -9,11 +9,15 @@ export class ClassModalComponent implements OnInit {
 
   @Input() private subject: Subject;
 
+  private statusOpen: boolean = false;
+  private statusClose: boolean = false;
+
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) {
     this.subject = data.class;
   }
 
-  ngOnInit() { }
+  ngOnInit() { 
+  }
 
   /**
    * Convierte na hora en milisegundos a un string en formano hh:mm
@@ -25,7 +29,22 @@ export class ClassModalComponent implements OnInit {
     return new Date(dateText);
   }
 
+  /**
+   * Esta función convierte el color según el estado de la materia
+   * 
+   * @param input Estado de la materia
+   */
+  private colorStatus(input: string){
+    let colorStatusObject:any = document.getElementsByClassName('cls-mod-elements-line status');
+    var i = 0;
 
+    if(input == "abierta"){
+      colorStatusObject[i].style.color= '#50ac31';
+    } else if(input == "cerrada"){
+      colorStatusObject[i].style.color= '#ec660c';
+    }
+    return input;
+  }
 
 
 }
