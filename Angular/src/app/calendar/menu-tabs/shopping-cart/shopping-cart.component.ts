d592e693e5fd9253aa0ad2f997da456f54462f73
Jesús <jesuspinarte@hotmail.com>
2019-05-17 23:44:14 -0500

    FIX

--- Angular/src/app/calendar/menu-tabs/shopping-cart/shopping-cart.component.ts
+++ Angular/src/app/calendar/menu-tabs/shopping-cart/shopping-cart.component.ts
@@ -3,6 +3,7 @@ import { Input } from '@angular/core';
 import { Subject } from '../../../shared/model/Subject';
 import { Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
+import { ReadJsonFileService } from 'src/app/shared/read-json-file/read-json-file.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -14,10 +15,9 @@ export class ShoppingCartComponent implements OnInit {
   @Input() private altTitle: String;
   @Input() private classes: Subject[];
   @Output() removeSubject = new EventEmitter<string>();
-  constructor() { }
+  constructor(private readJSONFileService: ReadJsonFileService) { }
 
   ngOnInit() {
-   
   }
 
   private removeClass(id){
@@ -29,6 +29,104 @@ export class ShoppingCartComponent implements OnInit {
       return word;
     }
     return word[0].toUpperCase() + word.substr(1).toLowerCase();
+  }
+
+  private enrollClasses() {
+    let classCodes = this.classes.map(({ numeroClase }) => numeroClase);
+    let enrollJson = {
+      "cicloLectivo": {
+        "codigo": this.getActualYearCicle() + ""
+      },
+      "clasesInscripcion": []
+    }
+    classCodes.forEach(code => enrollJson.clasesInscripcion.push(
+      {
+        "clase": {
+          "codigo": "" + code
+        },
+      }
+    ));
+
+    let user_token = null;
+    let response = {
+      "enrolled": [],
+      "failure": []
+    };
+    this.readJSONFileService.enrollClasses(enrollJson).subscribe(
+      recieved_token => {
+        user_token = recieved_token.token;
+      },
+      error => { },
+      () => {
+        this.readJSONFileService.enrollClassesService(enrollJson, user_token).subscribe(
+          answer => {
+            answer.clasesInscripcionEstudiante.forEach((mclass) => {
+              if (mclass.estado == "OK") {
+                response["enrolled"].push(mclass);
+              } else {
+                response["failure"].push(mclass);
+              }
+            });
+          },
+          error => {
+
+          },
+          () => {
+            this.printEnrollResults(response);
+          }
+        );
+      }
+    )
+  }
+    
+  private printEnrollResults(response){
+    let okClasses ="Se inscribieron correctamente: \n";
+    let failureClasses = "Fallo la inscripción de: \n";
+    response.enrolled.forEach(mClass => {
+      if(this.classes.find(subject => subject.numeroClase == mClass.clase.codigo) != undefined){
+        okClasses +=(this.classes.find(subject => subject.numeroClase == mClass.clase.codigo)).nombre + "\n";
+      }
+    });
+    response.failure.forEach(mClass => {
+      if(this.classes.find(subject => subject.numeroClase == mClass.clase.codigo) != undefined){
+        failureClasses +=(this.classes.find(subject => subject.numeroClase == mClass.clase.codigo)).nombre + "\n";
+      }
+    });
+    okClasses += "\n";
+    if(okClasses =="Se inscribieron correctamente: \n\n"){
+      okClasses = "";
+    }else if (failureClasses == "Fallo la inscripción de: \n"){
+      failureClasses = "";
+    }
+    alert(okClasses + failureClasses);
+
+  }
+
+
+  private getActualYearCicle() {
+    let currentdate = new Date();
+    let fullYear = currentdate.getFullYear();
+    let actualCycle: number;
+
+    if (currentdate.getMonth() < 6) {
+      actualCycle = 1;
+    } else if (currentdate.getMonth() == 6) {
+      if (currentdate.getDate() > 16) {
+        actualCycle = 2;
+      } else {
+        actualCycle = 1;
+      }
+    } else if (currentdate.getMonth() == 7) {
+      if (currentdate.getDate() < 14) {
+        actualCycle = 2;
+      } else {
+        actualCycle = 3;
+      }
+    } else if (currentdate.getMonth() >= 8) {
+      actualCycle = 3;
+    }
+
+    return fullYear + '-' + actualCycle
   }
 
 }
--- Angular/src/app/shared/read-json-file/read-json-file.service.ts
+++ Angular/src/app/shared/read-json-file/read-json-file.service.ts
@@ -12,6 +12,7 @@ import { Bloqueo } from '../model/rest/Bloqueo';
 import { BloqueoKey } from '../model/rest/keys/BloqueoKey';
 import { AlternativaKey } from '../model/rest/keys/AlternativaKey';
 import { Alternativa } from '../model/rest/Alternativa';
+import { endOfTomorrow } from 'date-fns';
 
 /**
  * Permite consumir servicios externos para leer archivos JSON
@@ -75,7 +76,6 @@ export class ReadJsonFileService {
         + '/' + filter['grade']
         + '/' + this.userToken.GID;
 
-      console.log(url);
       return (this.http.get<Subject[]>(url, { withCredentials: true }));
     }
 
@@ -238,4 +238,19 @@ export class ReadJsonFileService {
       return null;
     }
   }
+
+  public enrollClasses(enrollJson) : Observable<any> {
+    return this.getToken();
+    
+  }
+
+  public getToken() : Observable<any>{
+    return this.http.get<any>(this.baseUrl+"rest/get-token", { withCredentials: true, });
+  }
+
+  public enrollClassesService(enrollJson, user_token) : Observable<any>{
+    let body = user_token + " <--> " + JSON.stringify(enrollJson);
+    return this.http.post<any>(this.baseUrl + "rest/enroll-classes", body) ;
+  }
 }
+
